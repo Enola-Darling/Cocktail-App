5ec9a36bdf4b66f9b0476c9cf65ce21735be70ad
enola-darling <luna.raimondo@gmail.com>
2023-06-27 18:47:41 +0200

    feat: add product section in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ProductSection from "./components/productSection";
+import ProductList from "./components/ProductSection/ProductList/ProductList";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [productSection, setProductSection] = useState("");
 
   return (
-   <div className="App"></div>
-  )
+    <div className="App">
+      {productSection ? (
+        <ProductSection
+          productSection={productSection}
+          setProductSection={setProductSection}
+        />
+      ) : (
+        <>
+          <ProductList name="rum" setProductSection={setProductSection} />
+          <ProductList name="vodka" setProductSection={setProductSection} />
+          <ProductList name="gin" setProductSection={setProductSection} />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
